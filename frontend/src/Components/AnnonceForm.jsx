b3579e1e774c37d38080b0e8a17bb0c1300e1840
Zak-Hanfer <kz_hanfer@esi.dz>
2023-01-30 06:03:16 +0100

    Style poster

--- frontend/src/Components/AnnonceForm.jsx
+++ frontend/src/Components/AnnonceForm.jsx
@@ -4,6 +4,10 @@ import { useDropzone } from 'react-dropzone';
 
 const AnnonceForm = () => {
   const [title, setTitle] = useState('');
+  const [Lieu, setLieu] = useState('');
+  const [selectedCategorie, setSelectedCategorie] = useState('');
+  const [selectedTheme, setSelectedTheme] = useState('');
+  const [selectedModalit, setSelectedModalit] = useState('');
   const [selectedWilaya, setSelectedWilaya] = useState('');
   const [selectedCommune, setSelectedCommune] = useState('');
   const [Tarif, setTarif] = useState('');
@@ -32,6 +36,15 @@ const AnnonceForm = () => {
       'Zighoud Youcef'
     ],
   };
+  const handleCategorieChange = (event) => {
+    setSelectedCategorie(event.target.value);
+  }; 
+  const handleThemeChange = (event) => {
+    setSelectedTheme(event.target.value);
+  };  
+  const handleModalitChange = (event) => {
+    setSelectedModalit(event.target.value);
+  };  
     const handleWilayaChange = (event) => {
       setSelectedWilaya(event.target.value);
     };
@@ -49,93 +62,128 @@ const AnnonceForm = () => {
   };
 
   return (
-    <div className="bg-gray-200 py-10 px-5 rounded-lg">
+    <div className="bg-PrincipalCol ml-56 px-5 rounded-lg my-20 mx-20 w-2/3 rounded-xl">
         <Form onSubmit={handleSubmit}>
-        <FormGroup>
-                <Label for="title">Titre</Label>
-                <Input
-                    className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
-                    type="text"
-                    name="title"
-                    id="title"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    placeholder="Entrer un titre"
-                />
-        </FormGroup>
-        <FormGroup>
-                <Label for="Tarif">Tarif</Label>
-                <Input
-                    className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
-                    type="number"
-                    name="Tarif"
-                    id="Tarif"
-                    value={Tarif}
-                    onChange={e => setTarif(e.target.value)}
-                    placeholder="Entrer un Tarif"
-                />
-        </FormGroup>
-        <div className='grid-cols-3'>
-            <FormGroup>
-                    <Label for="Catégorie">Catégorie</Label>
-                    <select  className="text-black m-2">
-                        <option className='text-black ' value="" >Catégorie</option>
-                        <option value="Alger" className='text-black'>Science</option>
-                        <option value="Oran" className='text-black'>Physique</option>
-                        <option value="Constantine" className='text-black'>Mathématique</option>
-                        <option value="Alger" className='text-black'>Arabic</option>
-                        <option value="Oran" className='text-black'>Français</option>
-                        <option value="Constantine" className='text-black'>Anglais</option>
-                    </select>
-                    <label>Wilaya:</label>
-                    <select value={selectedWilaya} onChange={handleWilayaChange} className="text-black m-2">
-                        <option className='text-black ' value="" >Tous les wilayas</option>
-                        <option value="Alger" className='text-black'>Alger</option>
-                        <option value="Oran" className='text-black'>Oran</option>
-                        <option value="Constantine" className='text-black'>Constantine</option>
-                    </select>
-                    <label className='m-1'>Commune:</label>
-                    <select value={selectedCommune} onChange={handleCommuneChange} className="text-black m-2">
-                    <option value="">Toutes les communes</option>
-                    {selectedWilaya && communes[selectedWilaya].map((commune) => (
-                        <option key={commune} value={commune} className='text-black'>{commune}</option>
-                    ))}
-                    </select>
+        <div className=" ml-24">
+            <FormGroup className='py-4 grid grid-cols-3 justify-center items-center'>
+                    {/* <Label for="title">Titre</Label> */}
+                    <Input
+                        className="bg-white col-span-2 rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
+                        type="text"
+                        name="title"
+                        id="title"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        placeholder="Entrer un titre"
+                    />
             </FormGroup>
-        </div>
-        
-
-        <FormGroup>
-            <Label for="description" className="block text-gray-700 font-medium mb-2">Description</Label>
-            <textarea
-            className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
-            name="description"
-            id="description"
-            value={description}
-            onChange={e => setDescription(e.target.value)}
-            placeholder="Entrer une description"
-          />
-        </FormGroup>
-        <FormGroup>
-            <Label for="images" className="block text-gray-700 font-medium mb-2">Images</Label>
-            <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            <Button
-              className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
-            >
-                Télécharger des images
-            </Button>
+          
+            <div className='grid-cols-3'>
+                <FormGroup>
+                        {/* <Label for="Catégorie">Catégorie</Label> */}
+                        <select value={selectedCategorie} onChange={handleCategorieChange} className="text-black m-10 px-4 py-2  rounded-md shadow hover:bg-gray-100">
+                            <option className='text-black ' value="" >Catégorie</option>
+                            <option value="Alger" className='text-black'>Primaire</option>
+                            <option value="Oran" className='text-black'>Collège</option>
+                            <option value="Constantine" className='text-black'>Lycée</option>
+                            
+                        </select>
+                        {/* <Label for="Theme">Thème</Label> */}
+                        <select value={selectedTheme} onChange={handleThemeChange} className="text-black m-10 px-4 py-2 rounded-md shadow hover:bg-gray-100">
+                            <option className='text-black ' value="" >Thème</option>
+                            <option value="Alger" className='text-black'>Science</option>
+                            <option value="Oran" className='text-black'>Physique</option>
+                            <option value="Constantine" className='text-black'>Mathématique</option>
+                            <option value="Alger" className='text-black'>Arabic</option>
+                            <option value="Oran" className='text-black'>Français</option>
+                            <option value="Constantine" className='text-black'>Anglais</option>
+                        </select>
+                        {/* <Label for="Theme">Modalité</Label> */}
+                        <select value={selectedModalit} onChange={handleModalitChange} className="text-black m-10 px-4 py-2  rounded-md shadow hover:bg-gray-100">
+                            <option className='text-black ' value="" >Modalité</option>
+                            <option value="Alger" className='text-black'>Offline</option>
+                            <option value="Oran" className='text-black'>Online</option>
+                        </select>
+                        
+                </FormGroup>
             </div>
-            <div className="mt-4">
-            {images.map(image => (
-                <img key={image.path} src={image.path} alt={image.path} />
-            ))}
+            <FormGroup className='py-4 grid grid-cols-3 justify-center items-center'>
+                    <Label for="Tarif" className='bg-white rounded-lg border border-gray-300 py-2 px-4 block text-black focus:border-blue-500 w-32'>Tarif horaire</Label>
+                    <Input
+                        className="bg-white rounded-lg border border-gray-300 py-2 px-4 block  leading-5 text-gray-700 focus:outline-none focus:border-blue-500 w-56"
+                        type="number"
+                        name="Tarif"
+                        id="Tarif"
+                        value={Tarif}
+                        onChange={e => setTarif(e.target.value)}
+                        placeholder="Tarif/H"
+                    />
+            </FormGroup>
+            <div>
+              <FormGroup>
+                  {/* <Label className='flex'>Wilaya:</Label> */}
+                            <select value={selectedWilaya} onChange={handleWilayaChange} className="text-black m-10 px-4 py-2   justify-center items-center rounded-md shadow hover:bg-gray-100">
+                                <option className='text-black ' value="" >Wilaya</option>
+                                <option value="Alger" className='text-black'>Alger</option>
+                                <option value="Oran" className='text-black'>Oran</option>
+                                <option value="Constantine" className='text-black'>Constantine</option>
+                            </select>
+                            {/* <label className='m-1'>Commune:</label> */}
+                            <select value={selectedCommune} onChange={handleCommuneChange} className="text-black m-10 px-4 py-2   justify-center items-center rounded-md shadow hover:bg-gray-100">
+                            <option value="">Commune</option>
+                            {selectedWilaya && communes[selectedWilaya].map((commune) => (
+                                <option key={commune} value={commune} className='text-black'>{commune}</option>
+                            ))}
+                            </select>
+              </FormGroup>
             </div>
-        </FormGroup>
-        <Button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            Soumettre
-        </Button>
+            <FormGroup className='py-4 grid grid-cols-3 justify-center items-center'>
+                    {/* <Label for="title" className="block text-gray-700 font-medium mb-2">Lieu de la formation</Label> */}
+                    <Input
+                        className="bg-white col-span-2 rounded-lg border border-gray-300 py-2 px-4 block w-full leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
+                        type="text"
+                        name="lieu"
+                        id="lieu"
+                        value={Lieu}
+                        onChange={e => setLieu(e.target.value)}
+                        placeholder="Entres le lieu de la formation"
+                    />
+            </FormGroup>
+            <FormGroup>
+                {/* <Label for="description" className="block text-gray-700 font-medium mb-2">Description</Label> */}
+                <textarea
+                className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-4/6 leading-5 text-gray-700 focus:outline-none focus:border-blue-500 mt-5"
+                name="description"
+                id="description"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                placeholder="Entrer une description"
+              />
+            </FormGroup>
+            <FormGroup>
+                <Label for="images" className="block text-gray-700 font-medium mb-2 mt-5">Images</Label>
+                <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <Button
+                  className="bg-white rounded-lg border border-gray-300 py-2 px-4 block w-40 leading-5 text-gray-700 focus:outline-none focus:border-blue-500"
+                >
+                    Charger images
+                </Button>
+                </div>
+                <div className="mt-4">
+                {images.map(image => (
+                    <img key={image.path} src={image.path} alt={image.path} />
+                ))}
+                </div>
+            </FormGroup>
+            <div className='flex justify-center items-center '>
+                <Button type="submit" className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full my-5 ">
+                    Poster
+                </Button>
+            </div>
+            </div>
         </Form>
+        
         </div>
 );
 };
